295f613e0c9d9423374d15db84ac5fe809e0c740
Alan K <akillian@outlook.com>
2022-05-02 14:26:55 -0400

    commit

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -6,18 +6,11 @@
 
 <script setup>
 import { onMounted } from "vue";
-import { WORDS } from "@/components/modules/words.js";
 import init from "@/components/modules/initializeBoard.js";
 import KeyboardLayout from "@/components/KeyboardLayout.vue";
 import keystrokeHandler from "@/components/modules/keystrokeHandler.js";
 import "@/assets/css/gameBoard.css";
 
-const Number_Of_Guesses = 6;
-let guessesRemaining = Number_Of_Guesses;
-let currentGuess = [];
-let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
-console.log(rightGuessString);
 
 // Initialize Board
 onMounted(() => {
--- src/components/modules/keystrokeHandler.js
+++ src/components/modules/keystrokeHandler.js
@@ -1,5 +1,44 @@
+import { WORDS } from "@/components/modules/words.js";
+const Number_Of_Guesses = 6;
+let guessesRemaining = Number_Of_Guesses;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+console.log(rightGuessString);
+
 export default function () {
   document.addEventListener("keyup", (e) => {
-    console.log("I'm here ", e);
+    if (guessesRemaining === 0) {
+      return;
+    }
+
+    let pressedKey = String(e.key);
+    console.log(pressedKey);
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+      deleteLetter();
+      return;
+    }
+
+    if (pressedKey === "Enter") {
+      checkGuess();
+      return;
+    }
+
+    let found = pressedKey.match(/[a-z]/gi);
+    if (!found || found.length > 1) {
+      return;
+    } else {
+      insertLetter(pressedKey);
+    }
   });
+
+  // ========== Functions ======================== //
+
+  function insertLetter(pressedKey) {
+    if (nextLetter === 5) {
+      return;
+    }
+
+    pressedKey = pressedKey.toLowercase();
+  }
 }
