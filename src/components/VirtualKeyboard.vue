d2c05cfb40c514f2c58dca0e8a2930084cad1e38
Alan K <akillian@outlook.com>
2022-09-14 14:26:22 -0400

    commit

--- src/components/VirtualKeyboard.vue
+++ src/components/VirtualKeyboard.vue
@@ -24,7 +24,7 @@
       ><button class="keyboard-button">l</button>
     </div>
     <div class="third-row">
-      <button class="keyboard-button">Del</button
+      <button class="keyboard-button">Enter</button
       ><button class="keyboard-button">z</button
       ><button class="keyboard-button">x</button
       ><button class="keyboard-button">c</button
@@ -32,7 +32,14 @@
       ><button class="keyboard-button">b</button
       ><button class="keyboard-button">n</button
       ><button class="keyboard-button">m</button
-      ><button class="keyboard-button">Enter</button>
+      ><button class="keyboard-button">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24">
+          <path
+            fill="var(--color-tone-1)"
+            d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
+          ></path>
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -48,13 +55,15 @@
     */
 const clickHandler = (e) => {
   const target = e.target;
+  console.log(target.tagName);
 
   if (!target.classList.contains("keyboard-button")) {
     return;
   }
   let key = target.textContent;
+  console.log(key);
 
-  if (key === "Del") {
+  if (!key) {
     key = "Backspace";
   }
 
