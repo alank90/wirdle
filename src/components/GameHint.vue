7cd3a8f5018dd0229570f471e0fa7f88a8539cd8
Alan Killian <akillian@outlook.com>
2022-09-26 18:27:48 -0400

    commit

--- src/components/GameHint.vue
+++ src/components/GameHint.vue
@@ -26,11 +26,13 @@ function hint() {
   let wirdleLetter = "";
   let index = null;
   let lastBoardRowIndex = null;
+  let checkHint = true;
+  let i = 0;
   // generate a random number 0-4
-  index = Math.floor(Math.random() * props.propRightGuessString.length);
+  index = randomNumber();
 
   // Grab a random letter from the wirdle for the hint
-  wirdleLetter = props.propRightGuessString[index];
+  wirdleLetter = props.propRightGuessString[index - 1];
 
   // Get the last GameBoard row from the DOM
   lastBoardRowIndex = -props.propGuessesRemaining + 6;
@@ -38,15 +40,54 @@ function hint() {
   gameBoardChildren = gameBoard.childNodes;
   currentRow = gameBoardChildren[lastBoardRowIndex];
   rowLetters = currentRow.childNodes;
-  console.log(wirdleLetter);
+  console.log(rowLetters[1].innerHTML);
 
-  // Loop thru Row letters
-  rowLetters.forEach(function (letter, index) {
-    console.log(letter.innerHTML);
-    if (letter.innerHTML === wirdleLetter) {
-      console.log(`This letter is in wirdle ${letter.innerHTML}`);
+  while (checkHint) {
+    // In this loop we will check the css background color
+    // to see if it is green. If it is not we will print out
+    // the hint. If it is green we will get another letter from
+    // last guess(rowLetters) and check it and so on...
+    console.log(`Loop ======> ${i}`);
+    console.log(`The random index number is ${index}`);
+    console.log(
+      `The guessed Letter is: ${rowLetters[index].innerHTML.toUpperCase()}`
+    );
+    console.log(`The wirdle letter is ${wirdleLetter.toUpperCase()}`);
+
+    // rowLetters[index] should be a ternary rowletters[index ? index : 1] if zero
+    // make index 1 because rowLetters is a nodeList which is not zero-based
+    if (
+      rowLetters[index].innerHTML.toUpperCase() !== wirdleLetter.toUpperCase()
+    ) {
+      console.log(
+        `The hint letter ${rowLetters[index].innerHTML} is in position ${index}`
+      );
+      checkHint = false; // break while loop
+      console.log(`Breaking out of loop checkHint is ${checkHint}`);
+    } else {
+      // if we already have correct position of current letter
+      // let's get another wirdle letter and check it's status
+      index = randomNumber();
+      wirdleLetter = props.propRightGuessString[index - 1];
+      console.log(`New wirdle letter ${wirdleLetter} & position ${index}`);
+      i++;
+      // Safety valve. If we checked more then 10 times
+      // something I think went wrong.
+      if (i > 10) {
+        checkHint = false;
+      }
     }
-  });
+  }
+
+  console.log("OK out of loop..");
+}
+
+/**
+ * Description - Simple random number generator for numbers 0-4
+ *
+ */
+function randomNumber() {
+  return Math.floor(Math.random() * props.propRightGuessString.length);
 }
 </script>
 
